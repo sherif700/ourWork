5bc11de268a6b053ca1bd9432ab97a03285465c3
sherif <sherif.elkhiat.700@gmail.com>
2016-01-30 23:23:33 +0200

    admin control v.5

--- product.php
+++ product.php
@@ -1,12 +1,13 @@
 <?php
 class product
 {
-	var $id;
+	var $product_id;
 	var $product_name;
 	var $product_desc;
 	var $product_price;
 	var $product_quantity;
 	var $sub_cat_id;
+	var $category_id;
 
 	private static $conn = Null;
 	function __construct($id=-1) {
@@ -14,15 +15,16 @@ class product
 			self::$conn = mysqli_connect('localhost','root','iti','E-commerce');
 		}
 		if($id!=-1) {
-			$query = "select * from users where id=$id limit 1";
+			$query = "select * from products where product_id=$id limit 1";
 			$result = mysqli_query(self::$conn,$query);
 			$user = mysqli_fetch_assoc($result);
-			$this->id = $user['id'];
+			$this->product_id = $user['product_id'];
 			$this->product_name = $user['product_name'];
 			$this->product_desc = $user['product_desc'];
 			$this->product_price = $user['price'];
 			$this->product_quantity = $user['quantity'];
 			$this->sub_cat_id = $user['sub_cat_id'];
+			$this->category_id = $user['category_id'];
 		}
 	}
 
@@ -30,7 +32,7 @@ class product
 
 	function insert()
 	{
-		$query = "insert into users(product_name, product_desc ,product_price ,product_quantity ,sub_cat_id,category_id) 
+		$query = "insert into products(product_name, product_desc ,price ,quantity ,sub_cat_id,category_id) 
 		values('$this->product_name','$this->product_desc','$this->product_price','$this->product_quantity','$this->sub_cat_id','$this->category_id')";
 		$result  = mysqli_query(self::$conn,$query);
 		return mysqli_insert_id(self::$conn);
@@ -38,15 +40,16 @@ class product
 
 
 	function update() {
-		$query = "update users set product_name='$this->product_name', product_desc='$this->product_desc' , 
-		product_price='$this->product_price',product_quantity='$this->product_quantity',sub_cat_id='$this->sub_cat_id',category_id='$this->category_id' where id='$this->id'";
+		$query = "update products set product_name='$this->product_name', product_desc='$this->product_desc' , 
+		price='$this->product_price',quantity='$this->product_quantity',sub_cat_id='$this->sub_cat_id',category_id='$this->category_id' where product_id='$this->product_id'";
 		mysqli_query(self::$conn,$query);
 	}
 
 	function getbysubcat($sub_cat)
 	{
-		$query="select * from products where sub_cat_id = $sub_cat";
-		$result = mysql_query(self::$conn,$query);
+		$query="select product_name , price , image_path from products , gallery where 
+		gallery.product_id = products.product_id and sub_cat_id = $sub_cat";
+		$result = mysqli_query(self::$conn,$query);
 		while($row = mysqli_fetch_assoc($result)) {
 			$data[] = $row;
 		}
@@ -55,15 +58,22 @@ class product
 
 	function getbycat($cat)
 	{
-		$query="select * from products where category_id = $cat";
-		$result = mysql_query(self::$conn,$query);
+		$query="select product_name , price , image_path from products , gallery where 
+		gallery.product_id = products.product_id and category_id = $cat";
+		$result = mysqli_query(self::$conn,$query);
 		while($row = mysqli_fetch_assoc($result)) {
 			$data[] = $row;
 		}
 		return $data;
 	}
 
-	function product() {
+	function delete()
+	{
+		$query ="delete from products where product_id = '$this->product_id'";
+		mysqli_query(self::$conn,$query);
+	}
+
+	function products() {
 		$query = "select * from products";
 		$result = mysqli_query(self::$conn,$query);
 		$data = [];
@@ -85,10 +95,5 @@ class product
 		}
 		return $data;
 	}
-
-
-
-
-
 }
 ?>
